a02a9ce3e35d6f2dedd6c77d24420f7ef48bcf78
Pranali Dhangar <143471649+xdpranali@users.noreply.github.com>
2025-06-27 09:21:35 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 //Define the MongoDB connection URL
-//const mongoURL = 'mongodb://localhost:27017/hotels'
 const mongoURL = process.env.MONGODB_URL;
 
 //set up MongoDB connection
